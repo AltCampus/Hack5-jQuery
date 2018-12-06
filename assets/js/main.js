f0fb4ea2064ab22862b343974cda31576b7ca847
Ravi Yadav <1994.ravi@gmail.com>
2018-12-06 19:47:00 +0530

    add removeClass(), attr(), removeAttr(), and css() functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,9 +15,46 @@ const wrap = (els) => ({
       els.forEach(el => {
         el.classList.add(className)
       })
-    }
-  
-  
+    },
+
+    removeClass: (className) => {
+        els.forEach(el => {
+          el.classList.remove(className)
+        })
+      },
+
+    // hasClass: (className) => {
+    //     els.forEach(el => {
+    //       if(el.classList.contains(className)) return true;
+    //       else return false; 
+    //     })
+    //  },
+      attr: (key, value) => {
+        els.forEach(el => {
+          el.setAttribute(key, value);
+        })
+      },
+
+      removeAttr: (key, value) => {
+        els.forEach(el => {
+          el.removeAttribute(key, value);
+        })
+      },
+
+      css: (attribute, value) => {
+        els.forEach(element => {
+          element.style[attribute] = value;
+        })
+      },
+
+    //   css = function(attribute, value) {
+    //     var el = document.querySelectorAll(this.context);
+    //     el.forEach(element => {
+    //       element.style[attribute] = value;
+    //     })
+    //   },
+
+
   })
   
-  const $ = (els) => wrap(document.querySelectorAll(els))
+  const $u = (els) => wrap(document.querySelectorAll(els))
