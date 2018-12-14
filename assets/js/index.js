b0ca865212c15cd282815500e4a82d79ee644950
Ruchi <ruchikharwar4@gmail.com>
2018-12-14 10:16:52 +0530

    added children,find,val,toggleClass

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@ class Wrap{
   constructor(els) {
     this.els = els;
   }
+
   append(newHTML) {
     this.els.forEach(el => {
       let newEle = document.createElement(newHTML);
@@ -9,18 +10,22 @@ class Wrap{
     });
     return this;
   }
+
+
   addClass(className) {
     this.els.forEach(el => {
       el.classList.add(className)
     })
     return this;
   }
+
   removeClass(className) {
     this.els.forEach(el => {
       el.classList.remove(className)
     })
     return this;
   }
+
   hasClass(className) {
     this.els.forEach(el => {
       if (el.classList.contains(className)) {
@@ -44,20 +49,24 @@ class Wrap{
     })
     return this;
   }
+
   css(attribute, value) {
     this.els.forEach(element => {
       element.style[attribute] = value;
     })
     return this;
   }
+
   first(attribute, value) {
     this.els[0].style[attribute] = value;
     return this;
   }
+
   prop(attribute) {
     (this.els[0].getAttribute(attribute));
     return this;
   }
+
   html(innerText) {
     this.els.forEach(el => {
       if (!innerText) {
@@ -68,6 +77,41 @@ class Wrap{
     })
     return this;
   }
+
+  find(tag){
+    this.els.forEach(el => {
+      tag = el;
+      console.log(el) 
+    })
+    return this;
+  }
+
+  children(className){
+    this.els.forEach(el => {
+      className = el
+      console.log(el) 
+    })
+    return this;
+  }
+
+  val(newVal){
+    this.els.value = newVal;
+    // (newVal !== undefined ? this.els.value = newVal : this.els.value);
+    return this;
+  }
+
+toggleclass(className, status){
+  this.els.forEach(el => {
+      if (el.classList)
+      el.classList.add(className);
+    else
+      el.className += ' ' + className;
+
+    })
+  return this;
 }
+
+}
+
 
 let $ = (v) => new Wrap(document.querySelectorAll(v))
